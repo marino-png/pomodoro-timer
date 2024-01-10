b15c124f61a6a799eb4b2abbbbadc51e4b6f4f01
marino-png <marino@calcagni.eu>
2024-01-10 11:56:38 +0000

    pause feature implemented by clicking a secod time on the start buttun

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,12 @@
 const startTimerBtn = document.getElementById("start-timer-btn")
+
 startTimerBtn.addEventListener("click", () => {
-    chrome.storage.local.set({
-        isRunning: true,
+    chrome.storage.local.get(['isRunning'], (res) => {
+        chrome.storage.local.set({
+            isRunning: !res.isRunning,
+        }), () => {
+            startTimerBtn.textContent = !res.isRunning ? "Pause Timer" : "Start Timer"
+        }
     })
+    
 })
